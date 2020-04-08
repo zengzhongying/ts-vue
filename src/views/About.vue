ba68a47427a59afad96f652cdb516202a2fb19b9
zy.zeng <1363421830@qq.com>
2020-04-08 18:11:00 +0800

    初次提交

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <input type="text" v-model="name" />
+    <p>{{nameLength}}</p>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component /*, Watch*/ } from "vue-property-decorator";
+@Component({
+  // name: "About"
+})
+export default class About extends Vue {
+  private name = "zzy";
+
+  get nameLength(): number {
+    return this.name.length;
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <el-input type="text" v-model="myname"></el-input> -->
+    <el-form :model="form">
+      <el-form-item label="item1">
+        <el-input v-model="form.familyName" placeholder="请输入内容"></el-input>
+      </el-form-item>
+      <el-form-item label="item2">
+        <el-input v-model="form.uid" placeholder="请输入内容"></el-input>
+      </el-form-item>
+      <el-form-item label="item3">
+        <el-input v-model="form.nickName" placeholder="请输入内容"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-button @click="submitForm">提交</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import { Vue, Component /*, Watch*/ } from "vue-property-decorator";
+//構建表單結構體
+interface Form {
+  familyName: string;
+  uid: number;
+  nickName: string;
+}
+@Component({
+  name: "Home"
+})
+export default class Home extends Vue {
+  private myname = "嘻嘻哈哈";
+  private form: Form = {
+    familyName: "zzyFamily",
+    uid: 997,
+    nickName: "莫離."
+  };
+  private submitForm(): void {
+    console.log(this.form);
+  }
+  get myNameLength(): number {
+    return this.myname.length;
+  }
+
+  // @Watch("myNameLength")
+  // getmyNameLength(ov: number, nv: number) {
+  //   if (ov !== nv) {
+  //     console.log(ov, nv, "+++++");
+  //   }
+  // }
+}
+</script>
